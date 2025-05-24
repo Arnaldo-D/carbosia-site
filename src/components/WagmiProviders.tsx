0a74ec5bceb671007e543a333ced1dec4e75d712
Arnaldo Doricchi <arnaldo.doricchi@gruppofrandi.com>
2025-05-24 18:13:37 +0200

    fix: wrap wagmi with QueryClientProvider

--- src/components/WagmiProviders.tsx
+++ src/components/WagmiProviders.tsx
@@ -2,8 +2,20 @@
 
 import { WagmiConfig } from 'wagmi';
 import { wagmiConfig } from '@/lib/wagmi';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import type { ReactNode } from 'react';
 
+const queryClient = new QueryClient();
+
 export default function WagmiProviders({ children }: { children: ReactNode }) {
-  return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>;
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiConfig>
+  );
 }
