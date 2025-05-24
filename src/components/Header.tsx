3bba73560d7eb0fa9cbb6eb4c47d489411dd23e1
Arnaldo Doricchi <arnaldo.doricchi@gruppofrandi.com>
2025-05-24 06:23:02 +0200

    feat: create components folder and add Header with Blog link

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="w-full border-b bg-white">
+      <div className="mx-auto flex max-w-5xl items-center justify-between p-4">
+        <Link href="/" className="text-xl font-bold text-brand">
+          Carbosia
+        </Link>
+
+        <nav className="space-x-4">
+          <Link href="/about">About</Link>
+          <Link href="/blog">Blog</Link> {/* nuovo link */}
+          <Link href="/projects/sample-project">Projects</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
