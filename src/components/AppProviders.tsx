16d0f3a582059b46e608f9a89ab1b06af6e2592e
Arnaldo-D <149293957+Arnaldo-D@users.noreply.github.com>
2025-06-06 19:58:40 +0200

    feat: add react-query provider

--- src/components/AppProviders.tsx
+++ src/components/AppProviders.tsx
@@ -3,7 +3,10 @@
 import { ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
 import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
-import "@rainbow-me/rainbowkit/styles.css";          // <-- css globale RainbowKit
+import "@rainbow-me/rainbowkit/styles.css"; // <-- css globale RainbowKit
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 // chain che stai usando
 import { polygonAmoy } from "wagmi/chains";
@@ -21,9 +24,11 @@ export const wagmiConfig = getDefaultConfig({
 export default function AppProviders({ children }: { children: ReactNode }) {
   return (
     <WagmiProvider config={wagmiConfig}>
-      <RainbowKitProvider chains={[polygonAmoy]}>
-        {children}
-      </RainbowKitProvider>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider chains={[polygonAmoy]}>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
